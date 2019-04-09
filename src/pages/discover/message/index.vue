68d33144ab4d3c3f4f09c436e3150b265200e2f6
FEerllx <931863856@qq.com>
2019-04-09 23:07:37 +0800

    消息提醒

---
+++ src/pages/discover/message/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+   <div class="box plr10 pt15">
+     <div class="item dis-flex bg-fff plr20 pt15 pb10">
+       <div class="flex-1">
+         <div class="f17 mb15 cblack strong">某某产品预约已通过审核</div>
+         <div class="f13 cgey">发送时间：2019-03-20 15:52</div>
+       </div>
+       <div class="f13 clink">未读</div>
+     </div>
+     <div class="item"></div>
+     <div class="item"></div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+.box .item{
+  
+  background-color: #fff;
+}
+</style>
