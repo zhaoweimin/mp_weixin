e38b51a9a12e58bae2494ca154bf9de204e30718
FEerllx <931863856@qq.com>
2019-04-10 22:46:54 +0800

    消息提醒

---
+++ src/pages/discover/market/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main">
+   <div class="plr10 pt20">
+     <div class="card bg-fff dis-flex l-end">
+       <div class="card-left dis-flex flex-1 l-center a-center">
+         <div class="dis-flex flex-column a-center l-center">
+           <div class="strong f17 cblack mb20">可邀约活动</div>
+           <span class="f12 clink btn-in">点击进入</span>
+         </div>
+       </div>
+        <img class="card-img" src="../../../img/follow_1.png" alt="">   
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+.card{
+  height:150px;
+  box-shadow:0px 1px 9px 0px rgba(85,168,255,0.2);
+  border-radius:0px 0px 2px 2px;
+}
+.card-left{
+  height:150px;
+}
+.card-img{
+  width: 155px;
+  height:125px;
+}
+.btn-in{
+  border: 1px solid #509EF0;
+  border-radius: 50px;
+  padding: 2px 13px;
+}
+</style>
--- src/pages/discover/message/index.vue
+++ src/pages/discover/message/index.vue
@@ -6,10 +6,11 @@
          <div class="f17 mb15 cblack strong">某某产品预约已通过审核</div>
          <div class="f13 cgey">发送时间：2019-03-20 15:52</div>
        </div>
-       <div class="f13 clink">未读</div>
+       <div class="dis-flex flex-column l-end">
+         <div class="f13 clink">未读</div> 
+         <div class="f13 clink btn-detail clink mt15">前往查看</div> 
+        </div>
      </div>
-     <div class="item"></div>
-     <div class="item"></div>
    </div>
   </div>
 </template>
@@ -30,8 +31,13 @@ export default {
 </script>
 
 <style scoped>
-.box .item{
-  
-  background-color: #fff;
+.item{
+  box-shadow:0px 1px 9px 0px rgba(85,168,255,0.2);
+  border-radius: 2px;
+}
+.btn-detail{
+  padding: 2px 8px;
+  border-radius: 50px;
+  border: 1px solid #509EF0;
 }
 </style>
