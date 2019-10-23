376444fbdf4b3b6d457d6c49f635c5e1d8b51694
lulx <631452494@qq.com>
2019-10-23 20:01:47 +0800

    阿斯蒂芬爱的色放

--- src/pages/discover/message/index.vue
+++ src/pages/discover/message/index.vue
@@ -1,43 +1,41 @@
 <template>
-  <div class="main">
-   <div class="box plr10 pt15">
-     <div class="item dis-flex bg-fff plr20 pt15 pb10">
-       <div class="flex-1">
-         <div class="f17 mb15 cblack strong">某某产品预约已通过审核</div>
-         <div class="f13 cgey">发送时间：2019-03-20 15:52</div>
-       </div>
-       <div class="dis-flex flex-column l-end">
-         <div class="f13 clink">未读</div> 
-         <div class="f13 clink btn-detail clink mt15">前往查看</div> 
+    <div class="main">
+        <div class="box plr10 pt15">
+            <div class="item dis-flex bg-fff plr20 pt15 pb10">
+                <div class="flex-1">
+                    <div class="f17 mb10 cblack strong">管理员</div>
+                    <div class="f12 cblack mb5">客户：张天佑成为潜在客户</div>
+                    <div class="f12 cgey">2019-03-20 15:52</div>
+                </div>
+                <!-- <div class="dis-flex flex-column l-end">
+                    <div class="f13 clink">未读</div>
+                    <div class="f13 clink btn-detail clink mt15">前往查看</div>
+                </div> -->
+            </div>
         </div>
-     </div>
-   </div>
-  </div>
+    </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {
-    }
+    return {}
   },
 
-  methods: {
-  },
+  methods: {},
 
-  created () {
-  }
+  created () {}
 }
 </script>
 
 <style scoped>
-.item{
-  box-shadow:0px 1px 9px 0px rgba(85,168,255,0.2);
-  border-radius: 2px;
+.item {
+	box-shadow: 0px 1px 9px 0px rgba(85, 168, 255, 0.2);
+	border-radius: 2px;
 }
-.btn-detail{
-  padding: 2px 8px;
-  border-radius: 50px;
-  border: 1px solid #509EF0;
+.btn-detail {
+	padding: 2px 8px;
+	border-radius: 50px;
+	border: 1px solid #509ef0;
 }
 </style>
