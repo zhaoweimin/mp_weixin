aeb7488af5946953707fa401519b8a1b7388535a
zhaoweimin <464073319@qq.com>
2019-10-29 21:08:22 +0800

    question

---
+++ src/components/questionnaire.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="z-question">
+    <div class="quest">
+      {{sort}}. {{data.question}}（<span class="val">{{val}}</span>）
+    </div>
+    <div class="answer">
+      <div class="item" v-for="(vo, key) in data.options" :key="key" @click="choose(vo.val)">
+        <div class="val" :class="{active: val == vo.val}">{{vo.val}}</div>
+        <div class="text">{{vo.text}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['data', 'sort', 'sign'],
+  data() {
+    return {
+      val: ''
+    };
+  },
+  mounted () {
+  },
+  methods: {
+    choose (val) {
+      this.val = val
+      this.change();
+    },
+    change () {
+      this.$emit('change', this.val, this.sign);
+    }
+  }
+};
+</script>
+<style lang="less">
+.z-question{
+  padding: 15px;
+
+  .quest {
+    .val{
+      display: inline-block;
+      min-width: 20px;
+      text-align: center;
+    }
+  }
+
+  .answer {
+    padding-top: 10px;
+    .item{
+      display: flex;
+      margin-bottom: 10px;
+      .val{
+        width: 20px;
+        height: 20px;
+        line-height: 20px;
+        text-align: center;
+        margin-right: 10px;
+
+        &.active{
+          background: #57befd;
+          color: #fff;
+          border-radius: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/customer/quetions/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main">
+    <div class="block" @click="create(1)">
+      <img src="../../../img/personal_creat.jpg" alt="" mode="aspectFit">
+    </div>
+    <div class="block" @click="create(2)">
+      <img src="../../../img/company_creat.jpg" alt="" mode="aspectFit">
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar'
+import fliter from '@/components/fliterLine'
+import card from '@/components/customerCard'
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+    navbar,
+    fliter,
+    card
+  },
+
+  methods: {
+    create (type) {
+      let url = `../add/main?type=${type}`
+      mpvue.navigateTo({ url })
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.block{
+  margin: 15px;
+  width: 345px;
+  border-radius: 6px;
+  overflow: hidden;
+}
+.block img{
+  width: 100%;
+  height: 120px;
+}
+</style>
