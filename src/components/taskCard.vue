37957283c7c64cc056050c32044930b655469d80
lulx <631452494@qq.com>
2019-10-31 00:54:12 +0800

    待办修改

--- src/components/taskCard.vue
+++ src/components/taskCard.vue
@@ -1,36 +1,45 @@
 <template>
-  <div class="contract-card task-card" @click="detail(id)">
-    <div class="order"><div class="bg">合同编号：201903201530326548</div></div>
-    <div class="msg no-boder">
-      <div class="line">
-        <span class="key">类别：沟通</span>
-      </div>
-      <div class="line">
-        <span class="key">主题：<span class="cblue">提成申请</span></span>
-      </div>
-      <div class="line">
-        <span class="key">流程名称：提成申请</span>
-      </div>
-    </div>
-    <div class="msg">
-      <div class="line">
-        <span class="key">起草人：张瑶</span>
-      </div>
-      <div class="line">
-        <span class="key">发送人：张瑶</span>
-        <span class="time">登记时间：2019-03-16 10:30</span>
-      </div>
-    </div>
+    <div class="contract-card task-card" @click="detail(info.id)">
+        <div class="order">
+            <div class="bg">合同编号：201903201530326548</div>
+        </div>
+        <div class="msg no-boder">
+            <div class="line">
+                <span class="key">类别：沟通</span>
+            </div>
+            <div class="line">
+                <span class="key">主题：<span class="clink">{{info.theme}}</span></span>
+            </div>
+            <div class="line">
+                <span class="key">流程名称：{{info.theme}}</span>
+            </div>
+        </div>
+        <div class="msg">
+            <div class="line">
+                <span class="key">起草人：张瑶</span>
+            </div>
+            <div class="line">
+                <span class="key">发送人：张瑶</span>
+                <span class="time">登记时间：2019-03-16 10:30</span>
+            </div>
+        </div>
 
-  </div>
+    </div>
 </template>
 
 <script>
 export default {
-  props: ['text'],
+  props: {
+    info: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
   methods: {
     detail (id) {
-      let url = `/pages/task/detail/main?id=${id}`
+      let url = `/pages/task/detail${id}/main`
       mpvue.navigateTo({ url })
     }
   }
@@ -38,9 +47,9 @@ export default {
 </script>
 
 <style>
-.task-card .time{
-  float: right;
-  font-size: 10px;
-  color: #ccc;
+.task-card .time {
+	float: right;
+	font-size: 10px;
+	color: #ccc;
 }
 </style>
